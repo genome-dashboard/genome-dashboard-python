f6000750aa9b2b9faa7813d0fb5eec578947460d
jgentle <jgentle@tacc.utexas.edu>
2019-06-18 17:58:02 -0500

    Mpodule testing still. Ver 0.0.13.

--- genome_dashboard/__init__.py
+++ genome_dashboard/__init__.py
@@ -3,6 +3,8 @@
 """Top-level package for Genome Dashboard"""
 
 import core
+import genome_dashboard.hptools as hp
+import genome_dashboard.gdash as gdash
 
 
 __author__ = """Zilong Li, Ran Sun, Thomas C. Bishop"""
--- genome_dashboard/core.py
+++ genome_dashboard/core.py
@@ -2,9 +2,6 @@
 
 """Core module."""
 
-import genome_dashboard.hptools as hp
-import genome_dashboard.gdash as gdash
-
 
 def hello():
     print "Hello from the genome_dashboard.core.py file."
