0ae7721f47cb70dc753670796729cd2c1844c7af
jgentle <jgentle@tacc.utexas.edu>
2019-06-18 15:46:53 -0500

    Updated __init__.py for main module to reflect correct attrib info. Bump to ver0.0.8.

--- genome_dashboard/__init__.py
+++ genome_dashboard/__init__.py
@@ -2,6 +2,6 @@
 
 """Top-level package for Genome Dashboard."""
 
-__author__ = """My Full Name"""
+__author__ = """Zilong Li, Ran Sun, Thomas C. Bishop"""
 __email__ = 'genome.dashboard@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.0.8'
