a2fab96d4eaf57fb92ac79199156232d0f81753d
jgentle <jgentle@tacc.utexas.edu>
2019-06-18 17:50:00 -0500

    Teatsin gimports. Bumped to v0.0.11.

--- genome_dashboard/__init__.py
+++ genome_dashboard/__init__.py
@@ -2,5 +2,10 @@
 
 """Top-level package for Genome Dashboard"""
 
+import genome_dashboard.hptools as hp
+import genome_dashboard.gdash as gdash
+import core
+
+
 __author__ = """Zilong Li, Ran Sun, Thomas C. Bishop"""
 __email__ = 'genome.dashboard@gmail.com'
