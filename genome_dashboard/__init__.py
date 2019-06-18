c1f28e2280aa5c3f173cb3a36cab1aacdf1e4c2c
jgentle <jgentle@tacc.utexas.edu>
2019-06-18 18:04:35 -0500

    Nested modules.

--- genome_dashboard/__init__.py
+++ genome_dashboard/__init__.py
@@ -2,9 +2,12 @@
 
 """Top-level package for Genome Dashboard"""
 
-import core
-import genome_dashboard.hptools as hp
-import genome_dashboard.gdash as gdash
+# import core
+# import genome_dashboard.hptools as hp
+# import genome_dashboard.gdash as gd
+from core import *
+from hptools import *
+from gdash import *
 
 
 __author__ = """Zilong Li, Ran Sun, Thomas C. Bishop"""
