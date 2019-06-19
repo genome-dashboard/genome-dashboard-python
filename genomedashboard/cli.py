0fbba9056c150aa885a3df2231fca51061710b29
jgentle <jgentle@tacc.utexas.edu>
2019-06-19 16:54:27 -0500

    Tweaking tests.

--- genomedashboard/cli.py
+++ genomedashboard/cli.py
@@ -15,19 +15,5 @@ def main(args=None):
     return 0
 
 
-# if __name__ == "__main__":
-#     sys.exit(main())  # pragma: no cover
-
-
-@click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
-
-
-if __name__ == '__main__':
-    hello()
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
