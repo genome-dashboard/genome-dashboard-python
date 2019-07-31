2fa8af17224dea4615847722cbec1dcc13facde9
John Gentle <taoteg@gmail.com>
2019-07-31 14:06:55 -0500

    Updating packaging output.

--- genomedashboard/__init__.py
+++ genomedashboard/__init__.py
@@ -4,7 +4,7 @@
 
 # from os import path
 # from io import open
-from genomedashboard import genomedashboard as gd
+# from genomedashboard import genomedashboard as gd
 # import cli as cli
 from .convert import convert as cv
 from .ds import ds as ds
@@ -12,6 +12,6 @@ from .io import io as io
 
 
 # -->>> !!!! IMPORTANT: BUMP THE VERSION WITH EVERY COMMIT USING SEMVER CONVENTIONS  <Major.minor.patch> !!!! <<<--
-__version__ = '0.0.33'
+__version__ = '0.0.34'
 __author__ = 'Zilong Li, Ran Sun, Thomas C. Bishop'
 __email__ = 'genome.dashboard@gmail.com'
