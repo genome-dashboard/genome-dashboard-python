3aae406bcc72885bdac14163ee29aacb7460e04a
zli007 <38192974+zli007@users.noreply.github.com>
2019-09-14 20:11:16 -0500

    Update io.py
    debug

--- genomedashboard/io/io.py
+++ genomedashboard/io/io.py
@@ -103,13 +103,13 @@ class WRITE(object):
         f.write('       Shear  Stretch  Stagger Buckle Prop-Tw Opening   Shift  Slide    Rise    Tilt    Roll   Twist' + '\n')
         for i,j in enumerate(HP):
             f.write(seq[i]+' ')
-            if HP.HP_intra == None:
+            if j.HP_intra == None:
                 f.write(str("%0.2f" % 0.0).rjust(8)+str("%0.2f" % 0.0).rjust(8)+str("%0.2f" % 0.0).rjust(8)+str("%0.2f" % 0.0).rjust(8)+str("%0.2f" % 0.0).rjust(8)+str("%0.2f" % 0.0).rjust(8))
             else:
-                f.write(str("%0.2f" % HP.HP_intra.she).rjust(8)+str("%0.2f" % HP.HP_intra.str).rjust(8)+str("%0.2f" % HP.HP_intra.sta).rjust(8)+str("%0.2f" % HP.HP_intra.buc).rjust(8)+str("%0.2f" % HP.HP_intra.pro).rjust(8)+str("%0.2f" % HP.HP_intra.ope).rjust(8))
+                f.write(str("%0.2f" % j.HP_intra.she).rjust(8)+str("%0.2f" % j.HP_intra.str).rjust(8)+str("%0.2f" % j.HP_intra.sta).rjust(8)+str("%0.2f" % j.HP_intra.buc).rjust(8)+str("%0.2f" % j.HP_intra.pro).rjust(8)+str("%0.2f" % j.HP_intra.ope).rjust(8))
             if HP.HP_inter == None:
                 f.write(str("%0.2f" % 0.0).rjust(8)+str("%0.2f" % 0.0).rjust(8)+str("%0.2f" % 0.0).rjust(8)+str("%0.2f" % 0.0).rjust(8)+str("%0.2f" % 0.0).rjust(8)+str("%0.2f" % 0.0).rjust(8))
             else:
-                f.write(str("%0.2f" % HP.HP_intra.shi).rjust(8)+str("%0.2f" % HP.HP_intra.sli).rjust(8)+str("%0.2f" % HP.HP_intra.ris).rjust(8)+str("%0.2f" % HP.HP_intra.til).rjust(8)+str("%0.2f" % HP.HP_intra.rol).rjust(8)+str("%0.2f" % HP.HP_intra.twi).rjust(8))
+                f.write(str("%0.2f" % j.HP_inter.shi).rjust(8)+str("%0.2f" % j.HP_inter.sli).rjust(8)+str("%0.2f" % j.HP_inter.ris).rjust(8)+str("%0.2f" % j.HP_inter.til).rjust(8)+str("%0.2f" % j.HP_inter.rol).rjust(8)+str("%0.2f" % j.HP_inter.twi).rjust(8))
             f.write('\n')
         f.close()
