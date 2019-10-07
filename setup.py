4f7868b8eede34052084b8d54ef6c9b254d80a01
John Gentle <taoteg@gmail.com>
2019-10-07 16:51:47 -0500

    Updating build number.

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ print("... ASSIGNING CONFIGURATION VALUES ...\n")
 
 # -->>> !!!! IMPORTANT: BUMP THE VERSION WITH EVERY COMMIT USING SEMVER CONVENTIONS  <Major.minor.patch> !!!! <<<--
 # This value MUST be aligned with the value in .genome-dashboard-python/genomedashboard/__init__.py!!!
-module_version                          = '0.0.40'
+module_version                          = '0.0.41'
 
 module_name                             = 'genomedashboard'
 module_authors                          = 'Zilong Li, Ran Sun, Thomas C. Bishop'
