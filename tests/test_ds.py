57c5d63cfe7fd3bf08fa6ab3b8922a38befb62c1
zli007 <38192974+zli007@users.noreply.github.com>
2019-10-10 21:55:54 -0500

    Update test_ds.py

--- tests/test_ds.py
+++ tests/test_ds.py
@@ -3,31 +3,34 @@ import numpy as np
 from genomedashboard.ds import ds
 
 class Testds(unittest.TestCase):
+
+    def setUp(self):
+        self.hp_intra = ds.HP_intra(-0.32, -0.58, -0.34, -3.80, 4.48, -0.73)
+        self.hp_inter = ds.HP_inter(-0.87, -0.61, 3.07, 2.35, 0.78, 32.52)
+        
+    def tearDown(self):
+        pass
     
     def test_HP_intra(self):
-        hp_intra = ds.HP_intra(-0.32, -0.58, -0.34, -3.80, 4.48, -0.73)
-        self.assertEqual(hp_intra.she,-0.32)
-        self.assertEqual(hp_intra.str,-0.58)
-        self.assertEqual(hp_intra.sta,-0.34)
-        self.assertEqual(hp_intra.buc,-3.80)
-        self.assertEqual(hp_intra.pro,4.48)
-        self.assertEqual(hp_intra.ope,-0.73)
+        self.assertEqual(self.hp_intra.she,-0.32)
+        self.assertEqual(self.hp_intra.str,-0.58)
+        self.assertEqual(self.hp_intra.sta,-0.34)
+        self.assertEqual(self.hp_intra.buc,-3.80)
+        self.assertEqual(self.hp_intra.pro,4.48)
+        self.assertEqual(self.hp_intra.ope,-0.73)
         
     def test_HP_inter(self):
-        hp_inter = ds.HP_inter(-0.87, -0.61, 3.07, 2.35, 0.78, 32.52)
-        self.assertEqual(hp_inter.shi,-0.87)
-        self.assertEqual(hp_inter.sli,-0.61)
-        self.assertEqual(hp_inter.ris,3.07)
-        self.assertEqual(hp_inter.til,2.35)
-        self.assertEqual(hp_inter.rol,0.78)
-        self.assertEqual(hp_inter.twi,32.52)
+        self.assertEqual(self.hp_inter.shi,-0.87)
+        self.assertEqual(self.hp_inter.sli,-0.61)
+        self.assertEqual(self.hp_inter.ris,3.07)
+        self.assertEqual(self.hp_inter.til,2.35)
+        self.assertEqual(self.hp_inter.rol,0.78)
+        self.assertEqual(self.hp_inter.twi,32.52)
         
     def test_HP(self):
-        hp_intra = ds.HP_intra(-0.32, -0.58, -0.34, -3.80, 4.48, -0.73)
-        hp_inter = ds.HP_inter(-0.87, -0.61, 3.07, 2.35, 0.78, 32.52)
-        hp = ds.HP(hp_intra, hp_inter,hptype='3DNA')
-        self.assertIs(hp.HP_intra,hp_intra)
-        self.assertIs(hp.HP_inter,hp_inter)
+        hp = ds.HP(self.hp_intra, self.hp_inter,hptype='3DNA')
+        self.assertIs(hp.HP_intra,self.hp_intra)
+        self.assertIs(hp.HP_inter,self.hp_inter)
         self.assertEqual(hp.hptype,'3DNA')
         
     def test_RD(self):
