0a1a3f77b9be8bc164091b32224e0d13414bb1d9
zli007 <38192974+zli007@users.noreply.github.com>
2019-10-29 19:15:16 -0500

    Update convert.py

--- genomedashboard/convert/convert.py
+++ genomedashboard/convert/convert.py
@@ -451,7 +451,7 @@ def RD2SC(rd_list,hptype='3DNA',step_size=1):
         ty = np.diff(ry)/np.diff(s)
         tz = np.diff(rz)/np.diff(s)
         t = np.array([tx,ty,tz]).T
-        ssr = np.array([np.dot(t[i],rd_list[i].d.T) for i in range(len(s)-1)])
+        ssr = np.array([np.dot(t[i],rd_list[i+1].d.T) for i in range(len(s)-1)])
         d1x = np.array([x.d[0][0] for x in rd_list])
         d1y = np.array([x.d[0][1] for x in rd_list])
         d1z = np.array([x.d[0][2] for x in rd_list])
@@ -473,9 +473,9 @@ def RD2SC(rd_list,hptype='3DNA',step_size=1):
         deltad1 = np.array([td1x,td1y,td1z]).T
         deltad2 = np.array([td2x,td2y,td2z]).T
         deltad3 = np.array([td3x,td3y,td3z]).T
-        Twist = [np.dot(np.dot(deltad1[i],rd_list[i].d.T),np.array([0,1,0]))*180/np.pi for i in range(len(s)-1)]
-        Roll = [np.dot(np.dot(deltad3[i],rd_list[i].d.T),np.array([1,0,0]))*180/np.pi for i in range(len(s)-1)]
-        Tilt = [np.dot(np.dot(deltad2[i],rd_list[i].d.T),np.array([0,0,1]))*180/np.pi for i in range(len(s)-1)]
+        Twist = [np.dot(np.dot(deltad1[i],rd_list[i+1].d.T),np.array([0,1,0]))*180/np.pi for i in range(len(s)-1)]
+        Roll = [np.dot(np.dot(deltad3[i],rd_list[i+1].d.T),np.array([1,0,0]))*180/np.pi for i in range(len(s)-1)]
+        Tilt = [np.dot(np.dot(deltad2[i],rd_list[i+1].d.T),np.array([0,0,1]))*180/np.pi for i in range(len(s)-1)]
         hps=[ds.HP(ds.HP_intra(0.0,0.0,0.0,0.0,0.0,0.0),ds.HP_inter(0.0,0.0,0.0,0.0,0.0,0.0))]
         for i in range(len(s)-1):
             hps.append(ds.HP(ds.HP_intra(0.0,0.0,0.0,0.0,0.0,0.0),ds.HP_inter(ssr[i][0],ssr[i][1],ssr[i][2],Tilt[i],Roll[i],Twist[i])))
