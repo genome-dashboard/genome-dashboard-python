ad5a1f85668c5eb32b659bfa14340924618ff186
zli007 <38192974+zli007@users.noreply.github.com>
2019-10-31 16:38:38 -0500

    Update mathfunction.py

--- genomedashboard/mathfunction/mathfunction.py
+++ genomedashboard/mathfunction/mathfunction.py
@@ -66,7 +66,7 @@ def circular_DNA_RD(Radius, V1, V2, step_number, step_size=1.0):
     Twist = 360.0*V2/V1
     rd=[]
     for s in range(step_number):
-        r = np.array([Radius*np.cos(s*2*np.pi/V1),0,Radius*np.sin(s*2*np.pi/V1)])+np.array([Radius,0,0])
+        r = np.array([-Radius*np.cos(s*2*np.pi/V1),0,Radius*np.sin(s*2*np.pi/V1)])+np.array([Radius,0,0])
         d = np.dot(np.array([[np.cos(Twist*s),-np.sin(Twist*s),0],[np.sin(Twist*s),np.cos(Twist*s),0],[0,0,1]]),np.array([[np.cos(s*2*np.pi/V1),0,-np.sin(s*2*np.pi/V1)],[0,1,0],[np.sin(s*2*np.pi/V1),0,np.cos(s*2*np.pi/V1)]]))
         rd.append(ds.RD(r,d))
     return rd
