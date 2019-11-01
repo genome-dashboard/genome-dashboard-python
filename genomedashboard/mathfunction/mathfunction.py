873f7f5c850aa2c8d315824ca66d9caa1ea62811
zli007 <38192974+zli007@users.noreply.github.com>
2019-11-01 16:33:04 -0500

    Update mathfunction.py

--- genomedashboard/mathfunction/mathfunction.py
+++ genomedashboard/mathfunction/mathfunction.py
@@ -98,8 +98,8 @@ def helix_shear_RD(Rise,Twist,V1,V2,step_number,step_size):
     rd=[]
     for s in range(step_number):
         r = np.array([-Radius*np.cos(s*2*np.pi/V1),s*V2,Radius*np.sin(s*2*np.pi/V1)])+np.array([Radius,0,0])
-        phi = np.arctan(V2*s/Radius)
-        X = np.array([[np.cos(phi),-np.sin(phi),0],[np.sin(phi),np.cos(phi),0],[0,0,1]])
+        phi = np.arcsin(V2/Rise)
+        X = np.array([[1,0,0],[0,np.cos(s*phi),-np.sin(s*phi)],[0,np.sin(s*phi),np.cos(s*phi)]])
         Y = np.array([[np.cos(s*2*np.pi/V1),0,-np.sin(s*2*np.pi/V1)],[0,1,0],[np.sin(s*2*np.pi/V1),0,np.cos(s*2*np.pi/V1)]])
         Z = np.array([[np.cos(Twist*s),-np.sin(Twist*s),0],[np.sin(Twist*s),np.cos(Twist*s),0],[0,0,1]])
         d = np.dot(X,np.dot(Z.T,Y))
