518d1886411a079db65c096750d1ee2b17811618
zli007 <38192974+zli007@users.noreply.github.com>
2019-11-01 11:41:57 -0500

    Update mathfunction.py

--- genomedashboard/mathfunction/mathfunction.py
+++ genomedashboard/mathfunction/mathfunction.py
@@ -75,7 +75,7 @@ def helix_torsion_RD(Rise, Twist, V1, V2, step_number, step_size):
     V1 = V1/step_size
     V2 = V2*step_size
     Rise = Rise*step_size
-    Twist = Twist*step_size
+    Twist = Twist*step_size*np.pi/180
     Radius = np.sqrt(Rise**2/(4*(np.sin(np.pi/V1))**2+(np.tan(V2*np.pi/180))**2))
     h = Radius*np.tan(V2*np.pi/180)
     rd=[]
@@ -84,7 +84,7 @@ def helix_torsion_RD(Rise, Twist, V1, V2, step_number, step_size):
         phi = V2*np.pi/180
         Y = np.array([[np.cos(s*2*np.pi/V1),0,-np.sin(s*2*np.pi/V1)],[0,1,0],[np.sin(s*2*np.pi/V1),0,np.cos(s*2*np.pi/V1)]])
         Z = np.array([[np.cos((Twist-phi)*s),-np.sin((Twist-phi)*s),0],[np.sin((Twist-phi)*s),np.cos((Twist-phi)*s),0],[0,0,1]])
-        d = np.dot(Z,Y)
+        d = np.dot(Z.T,Y)
         rd.append(ds.RD(r,d))
     return rd
 
